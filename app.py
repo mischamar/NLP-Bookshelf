1b20a2ab408ee7b44f892602fffc839529205d18
Maria <83551206+maria-nlp@users.noreply.github.com>
2021-06-30 22:08:14 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+from PIL import Image
+import json
+import requests
+import cv2
+
+app = Flask(__name__)
+
+@app.route('/getbook', methods=['GET', 'POST'])
+def getbooktitle():
+    if request.method == "POST":
+        file = request.files['file']
+        if file:
+            image = Image.open(file)
+            booktitle = pytesseract.image_to_string(image)
+            return booktitle
+
+
+api_key = "AIzaSyDlp19ogXeUuugzO3UZYRUqL9RVSzo2nQk"
+
+url = f"https://www.googleapis.com/books/v1/volumes?q={booktitle}&key={api_key}"
+response = requests.get(url)
+json_response = response.json()
+volume_info = json_response['items'][0]['volumeInfo']
+api_title = volume_info.get('title')
+api_author = volume_info.get('authors')
+images_link = volume_info['imageLinks']
+small_image= images_link['smallThumbnail']
+data = {
+'title': title,
+'author': author,
+'images_links': images_link
+}
+data_in_json = json.dumps(data)
+
