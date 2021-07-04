3551c0708945f0c2d4cdede221686157ee3499ab
Maria <83551206+maria-nlp@users.noreply.github.com>
2021-07-04 21:31:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,28 @@
-from flask import Flask, request,
+from flask import Flask, request, jsonify
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 from PIL import Image
-import json
 import requests
-import cv2
+
 
 app = Flask(__name__)
 
-@app.route('/getbook', methods=['GET', 'POST'])
+@app.route("/getbook", methods=["POST"])
 def getbooktitle():
     if request.method == "POST":
-        file = request.files['file']
-        if file:
-            image = Image.open(file)
-            booktitle = pytesseract.image_to_string(image)
-            return booktitle
+        file = request.files['image']
+        img = Image.open(file.stream)
+        booktitle = pytesseract.image_to_string(img)
 
+        api_key = "AIzaSyDlp19ogXeUuugzO3UZYRUqL9RVSzo2nQk"
+        url = f"https://www.googleapis.com/books/v1/volumes?q={booktitle}&key={api_key}"
+        response = requests.get(url)
+        json_response = response.json()
+        volume_info = json_response['items'][0]['volumeInfo']
+        title = volume_info.get('title')
+        author = volume_info.get('authors')
+        images_link = volume_info['imageLinks']
+        return jsonify({'title': title,'author': author,'images_links': images_link})
 
-api_key = "AIzaSyDlp19ogXeUuugzO3UZYRUqL9RVSzo2nQk"
-
-url = f"https://www.googleapis.com/books/v1/volumes?q={booktitle}&key={api_key}"
-response = requests.get(url)
-json_response = response.json()
-volume_info = json_response['items'][0]['volumeInfo']
-api_title = volume_info.get('title')
-api_author = volume_info.get('authors')
-images_link = volume_info['imageLinks']
-small_image= images_link['smallThumbnail']
-data = {
-'title': title,
-'author': author,
-'images_links': images_link
-}
-data_in_json = json.dumps(data)
-
+if __name__ == "__main__":
+    app.run(debug=True)
